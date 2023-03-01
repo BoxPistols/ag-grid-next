a819de6f0fbd559be79ccfc2a88a6108fcbe1c8c
aito <BoxPistols@users.noreply.github.com>
2023-03-02 08:23:08 +0900

    build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  hostname: ["localhost:3000, github.io,vercel.app"],
+  port: "3000",
+  // domains: [""],
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
+
+// https://nextjs.org/docs/api-reference/next/image
