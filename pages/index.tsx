a2d54f86393213d544e73056d5a3228005f3afc5
aito <BoxPistols@users.noreply.github.com>
2023-03-02 06:09:41 +0900

    Base Table / WIP useState-Rule ignore one-time

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
+import React, { useState } from "react"
+import { AgGridReact } from "ag-grid-react"
+import "ag-grid-community/styles/ag-grid.css"
+import "ag-grid-community/styles/ag-theme-alpine.css"
 
 const index = () => {
+
+  const [rowData] = useState([
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxster", price: 72000 },
+  ])
+
+  const [columnDefs] = useState([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+  ])
+
   return (
-    <div>index</div>
+    <>
+      {
+        <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+          <AgGridReact
+          rowData={rowData}
+          columnDefs={columnDefs}></AgGridReact>
+        </div>
+      }
+    </>
   )
 }
 
