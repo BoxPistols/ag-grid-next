6638e5a36646654205b2496a6f567f102be71b69
aito <BoxPistols@users.noreply.github.com>
2023-03-25 04:16:18 +0900

    Add Storybook + Plugin + Setting MUI Emotion

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,23 @@
+import { CacheProvider, cache } from '@emotion/react';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../src/lib/theme';
+
+export const decorators = [
+  (Story) => (
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    </CacheProvider>
+  ),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
---
+++ src/lib/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {},
+  typography: {},
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+  zIndex: {},
+  components: {},
+});
