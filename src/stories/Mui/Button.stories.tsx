6bf0740515b939facbc343bb55c8dcff94304ecc
aito <BoxPistols@users.noreply.github.com>
2023-03-28 04:26:58 +0900

    SB to CSF 3.0

--- src/stories/Mui/Button.stories.tsx
+++ src/stories/Mui/Button.stories.tsx
@@ -1,46 +1,17 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Button, ButtonProps } from './Button';
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+import { Button } from './Button'
 
-export default {
-  title: 'Mui/MuiButton',
-  component: Button,
-  argTypes: {
-    color: {
-      control: {
-        type: 'radio',
-        options: ['primary', 'secondary', 'success', 'warning', 'error'],
-      },
-    },
+export default { component: Button } as ComponentMeta<typeof Button>
+
+export const Basic: ComponentStoryObj<typeof Button> = {
+  args: {
+    label: '保存する',
   },
-} as ComponentMeta<typeof Button>;
+}
 
-const Template: ComponentStory<typeof Button> = (args: ButtonProps) => (
-  <Button {...args} />
-);
-
-export const Primary = Template.bind({});
-Primary.args = {
-  label: '保存する',
-  color: 'primary',
-};
-
-export const Error = Template.bind({});
-Error.args = {
-  label: 'Button',
-  color: 'error',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-  color: 'primary',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-  color: 'primary',
-};
+export const Error: ComponentStoryObj<typeof Button> = {
+  args: {
+    label: 'エラー',
+    color: 'error',
+  },
+}
