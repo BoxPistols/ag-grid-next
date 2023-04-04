1d9651a32caf71762c760b185fe2970967bda723
aito <BoxPistols@users.noreply.github.com>
2023-04-05 05:48:13 +0900

    filter UI

--- pages/custom-filter.tsx
+++ pages/custom-filter.tsx
@@ -1,16 +1,13 @@
-import { useState, useEffect, useRef, useCallback } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { AgGridReact } from 'ag-grid-react'
 import { GridApi } from 'ag-grid-community'
-import { GridReadyEvent } from 'ag-grid-community'
+import { localeJa } from '@/assets/locale.ja'
 
-// AgGrid
 import 'ag-grid-enterprise'
 import 'ag-grid-community/styles/ag-grid.css'
 import 'ag-grid-community/styles/ag-theme-alpine.css'
-// import styles from '@/styles/Home.module.css'
 
 import TextField from '@mui/material/TextField'
-import { localeJa } from '@/assets/locale.ja'
 import { Autocomplete, Box } from '@mui/material'
 
 interface CountryData {
@@ -25,37 +22,40 @@ interface CountryData {
 
 const Filter = (): JSX.Element => {
   const [rowData, setRowData] = useState<CountryData[]>([])
-  const [isDataLoaded, setIsDataLoaded] = useState(false)
-  // ----- for MUI filter -----
   const [gridApi, setGridApi] = useState<GridApi | null>(null)
   const [selectedCountries, setSelectedCountries] = useState<string[]>([])
   const [allCountries, setAllCountries] = useState<string[]>([])
+  const [newConfirmedFilter, setNewConfirmedFilter] = useState({
+    filterType: null,
+    filter: null,
+    filterTo: null,
+  })
 
-  const gridApiRef = useRef<GridApi | null>(null)
+  const localeText = {
+    ...localeJa,
+  }
 
-  // Data
   const fetchData = useCallback(async () => {
     try {
       const response = await fetch('https://api.covid19api.com/summary')
       const data = await response.json()
       setRowData(data.Countries)
-      setIsDataLoaded(true)
       setAllCountries(
         data.Countries.map((country: CountryData) => country.Country)
       )
-      console.log('get data!!')
     } catch (error) {
       console.log('get data error... ' + error)
     }
   }, [])
+
   useEffect(() => {
     fetchData()
   }, [fetchData])
 
-  // ----- for MUI filter -----
-  const onGridReady = (event: GridReadyEvent) => {
+  const onGridReady = (event: any) => {
     setGridApi(event.api)
   }
+
   const applyFilterModel = (values: string[]) => {
     if (gridApi) {
       const filterModel = {
@@ -78,12 +78,36 @@ const Filter = (): JSX.Element => {
       gridApi.setFilterModel(filterModel)
     }
   }
-  // 選択解除後、再度全表示
+
   useEffect(() => {
     if (gridApi && selectedCountries.length === 0) {
       gridApi.setFilterModel(null)
     }
   }, [gridApi, selectedCountries])
+
+  const handleNewConfirmedFilterChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { name, value } = event.target
+    setNewConfirmedFilter((prevFilter) => ({ ...prevFilter, [name]: value }))
+  }
+
+  const applyNewConfirmedFilter = () => {
+    if (gridApi) {
+      const filterModel = {
+        NewConfirmed: {
+          filterType: 'number',
+          filter: newConfirmedFilter.filter,
+          filterTo: newConfirmedFilter.filterTo,
+        },
+      }
+      gridApi.setFilterModel(filterModel)
+    }
+  }
+
+  useEffect(() => {
+    applyNewConfirmedFilter()
+  }, [newConfirmedFilter, gridApi])
 
   const columnDefs = [
     { headerName: '国名', field: 'Country' },
@@ -127,58 +151,59 @@ const Filter = (): JSX.Element => {
   ]
 
   const defaultColDef = {
-    // defaultColDef
     filter: true,
-  }
-
-  const localeText = {
-    // localeText
-    ...localeJa,
-  }
-
-  const gridOptions = {
-    // gridOptions
-    maxColWidth: 200, // maxColumnWidth
-    autoSizePadding: 8, // autoSizePadding
   }
 
   return (
     <>
-      {/* // ----- for MUI filter ----- */}
-      <Autocomplete
-        multiple
-        options={allCountries}
-        onChange={filterByCountries}
-        renderInput={(params) => (
+      <Box p={4}>
+        <Autocomplete
+          multiple
+          options={allCountries}
+          onChange={filterByCountries}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="国名で検索"
+              variant="outlined"
+              style={{ marginBottom: '16px' }}
+            />
+          )}
+        />
+        <Box>
           <TextField
-            {...params}
-            label="国名で検索"
-            variant="outlined"
-            style={{ marginBottom: '16px' }}
+            name="filter"
+            label="新規感染者数 (最小値)"
+            type="number"
+            onChange={handleNewConfirmedFilterChange}
+            style={{ marginRight: '16px' }}
           />
-        )}
-      />
-      {isDataLoaded && (
+          <TextField
+            name="filterTo"
+            label="新規感染者数 (最大値)"
+            type="number"
+            onChange={handleNewConfirmedFilterChange}
+          />
+        </Box>
         <div
           className="ag-theme-alpine"
-          style={{ height: '700px', width: '100%' }}
+          style={{ height: '700px', width: '100%', marginTop: '16px' }}
         >
           <AgGridReact
             rowData={rowData}
             columnDefs={columnDefs}
             defaultColDef={defaultColDef}
+            onGridReady={onGridReady}
+            // add
             localeText={localeText}
             sideBar={true}
             enableCharts={true}
             enableRangeSelection={true}
             rowGroupPanelShow={'always'}
-            gridOptions={gridOptions}
-            // filter
-            onGridReady={onGridReady}
             onFirstDataRendered={onFirstDataRendered}
           />
         </div>
-      )}
+      </Box>
     </>
   )
 }
