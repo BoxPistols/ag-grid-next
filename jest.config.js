f57cfb81162f94c128fdd66db96aa993d3bad67d
aito <BoxPistols@users.noreply.github.com>
2023-04-10 04:08:05 +0900

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+};
