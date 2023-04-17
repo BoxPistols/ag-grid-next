228176fdeb4774ca74307a9b44a719c930caf575
aito <BoxPistols@users.noreply.github.com>
2023-04-18 06:13:50 +0900

    base move on Next.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import '@/styles/globals.css'
 import '@/assets/locale.ja.js'
 import { GlobalAppBar } from '@/src/content/GlobalAppBar'
 import { Sidebar } from '@/src/content/SideBar'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from '@/src/lib/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [open, setOpen] = useState(false)
@@ -13,9 +15,11 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <GlobalAppBar handleDrawerToggle={handleDrawerToggle} />
-      <Sidebar open={open} handleDrawerToggle={handleDrawerToggle} />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <GlobalAppBar handleDrawerToggle={handleDrawerToggle} />
+        <Sidebar open={open} handleDrawerToggle={handleDrawerToggle} />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   )
 }
---
+++ pages/design/index.tsx
@@ -0,0 +1,13 @@
+import Typography from '@mui/material/Typography'
+
+const design = () => {
+  return (
+    <>
+      <Typography variant="body1" color="primary">
+        AAAAAAああああ
+      </Typography>
+    </>
+  )
+}
+
+export default design
