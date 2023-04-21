5000ee201ec49697f886f253fe2ef6095106b14f
aito <BoxPistols@users.noreply.github.com>
2023-04-22 07:48:35 +0900

    set spacing

--- src/stories/theme/ResponsiveGrid.stories.tsx
+++ src/stories/theme/ResponsiveGrid.stories.tsx
@@ -1,14 +1,32 @@
-// src/stories/theme/ResponsiveGrid.stories.tsx
 import { Meta } from '@storybook/react'
-import ResponsiveGrid from './ResponsiveGrid'
+import { ResponsiveGrid } from './ResponsiveGrid'
+import Typography from '@mui/material/Typography'
 
 const ResponsiveGridStory: Meta = {
   title: 'Catalog/Grid',
   component: ResponsiveGrid,
   tags: ['autodocs'],
-  argTypes: {},
+  // argTypes: {},
+  argTypes: {
+    spacing: {
+      options: [1, 2, 3, 4, 8, 16, 24, 32],
+      control: {
+        type: 'select',
+      },
+      defaultValue: 4,
+    },
+  },
 }
 
 export default ResponsiveGridStory
 
-export const Default = () => <ResponsiveGrid />
+export const Default = (args: any) => {
+  return (
+    <>
+      <Typography variant="h4" gutterBottom>
+        Grid パターン レスポンシブ例
+      </Typography>
+      <ResponsiveGrid {...args} />
+    </>
+  )
+}
--- src/stories/theme/ResponsiveGrid.tsx
+++ src/stories/theme/ResponsiveGrid.tsx
@@ -1,19 +1,16 @@
-// src/stories/theme/ResponsiveGrid.tsx
-import React from 'react'
-import { useTheme } from '@mui/material/styles'
-import Grid from '@mui/material/Grid'
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
-import Typography from '@mui/material/Typography'
+import { Grid, Card, CardContent, Typography } from '@mui/material'
 
-const ResponsiveGrid: React.FC = () => {
-  const theme = useTheme()
-  const breakpoints = theme.breakpoints.values
+interface ResponsiveGridProps {
+  spacing: number
+}
 
+type Props = ResponsiveGridProps
+
+export const ResponsiveGrid = ({ spacing }: Props) => {
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={spacing}>
       {Array.from({ length: 12 }, (_, i) => (
-        <Grid item xs={12} sm={6} md={4} lg={2} key={i}>
+        <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={i}>
           <Card>
             <CardContent>
               <Typography variant="h6">Card {i + 1}</Typography>
@@ -24,5 +21,3 @@ const ResponsiveGrid: React.FC = () => {
     </Grid>
   )
 }
-
-export default ResponsiveGrid
