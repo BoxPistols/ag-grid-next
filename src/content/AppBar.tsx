7d24f0d0bebc5dcc0ec87f50056b1cb69c586a29
A_ito <BoxPistols@users.noreply.github.com>
2023-05-24 00:30:01 +0900

    re theme Add layout from CustoMui (#59)

--- src/content/AppBar.tsx
+++ src/content/AppBar.tsx
@@ -1,151 +1,54 @@
-import * as React from 'react'
-import {
-  Box,
-  Toolbar,
-  List,
-  Typography,
-  IconButton,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  //   ListItemButton,
-} from '@mui/material'
+import { Box, Toolbar, Typography, IconButton } from '@mui/material'
 
 import {
-  // Menu as MenuIcon,
   ChevronLeft as ChevronLeftIcon,
   ChevronRight as ChevronRightIcon,
 } from '@mui/icons-material'
-import { AppBar, Drawer, DrawerHeader } from './AppBarUtil'
-import { Home, TableChart, DataObject } from '@mui/icons-material'
-import Link from 'next/link'
 
-import { blueGrey } from '@mui/material/colors'
-import { grey } from '@mui/material/colors'
+import { AppBar } from './AppBarUtil'
+import { AppBarSideNav } from './AppBarSideNav'
 
-const primaryOverride = blueGrey[900]
+import theme from '../lib/theme'
+import { ReactNode, useState } from 'react'
 
 // Type
 type Props = {
-  children: React.ReactNode
+  children: ReactNode
 }
 
 export const AppBarHeader = ({ children }: Props) => {
-  // const theme = useTheme();
-  const [open, setOpen] = React.useState(true)
+  const [open, setOpen] = useState(true)
 
   const handleDrawerOpen = () => {
-    setOpen(true)
+    setOpen(!open)
   }
-
-  const handleDrawerClose = () => {
-    setOpen(false)
-  }
-
-  const menuItems = [
-    { icon: <Home />, text: 'ホーム', href: '/' },
-    // TODO: Nest
-    {
-      icon: <TableChart />,
-      text: 'データインポート',
-      href: '/table/file-upload',
-    },
-    {
-      icon: <TableChart />,
-      text: 'カスタムフィルター',
-      href: '/custom-filter',
-    },
-    {
-      icon: <TableChart />,
-      text: 'レコードイベント',
-      href: '/table/record',
-    },
-    { icon: <DataObject />, text: 'Next.js Local APIテスト', href: '/get-api' },
-    //   { icon: <Settings />, text: '設定' },
-    //   { icon: <Info />, text: '情報' },
-  ]
 
   return (
     <Box sx={{ display: 'flex' }}>
-      <AppBar
-        position="fixed"
-        sx={{
-          backgroundColor: primaryOverride,
-          height: 48,
-          display: 'flex',
-          justifyContent: 'center',
-          a: {
-            cursor: 'pointer',
-          },
-        }}
-      >
+      <AppBar position="fixed">
         <Toolbar>
           <IconButton
             color="inherit"
             aria-label="toggle drawer"
-            onClick={open ? handleDrawerClose : handleDrawerOpen}
+            onClick={handleDrawerOpen}
             edge="start"
-            sx={{
-              marginRight: 5,
-            }}
           >
             {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
           </IconButton>
-          <Typography variant="h4" noWrap component="div" color="white">
-            <Link href="/">React Data Grid MUI</Link>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            color={theme.palette.primary.contrastText}
+          >
+            AgGrid Next Mui
           </Typography>
         </Toolbar>
       </AppBar>
 
-      <Drawer variant="permanent" open={open}>
-        <List sx={{ py: 1, mt: 6 }}>
-          {menuItems.map((item, index) => (
-            <ListItem
-              key={index}
-              component={Link}
-              href={item.href}
-              sx={{
-                minHeight: 32,
-                justifyContent: open ? 'initial' : 'center',
-                px: 2,
+      <AppBarSideNav open={open} />
 
-                '&:hover': {
-                  backgroundColor: grey[300],
-                },
-                '.MuiListItemIcon-root': {
-                  minWidth: 'auto',
-                  mr: 1,
-                },
-              }}
-            >
-              <ListItemIcon
-                sx={{
-                  minWidth: 0,
-                  mr: open ? 1 : 'auto',
-                  justifyContent: 'center',
-                }}
-              >
-                {item.icon}
-              </ListItemIcon>
-              <ListItemText
-                primary={item.text}
-                sx={{
-                  display: open ? 'block' : 'none',
-                  opacity: open ? 1 : 0,
-                  '.MuiTypography-root': {
-                    fontSize: 13,
-                    whiteSpace: 'nowrap',
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                  },
-                }}
-              />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-
-      <Box component="main" sx={{ flexGrow: 1, py: 8, px: 3 }}>
+      <Box component="main" sx={{ flexGrow: 1, py: 6, px: 4 }}>
         {children}
       </Box>
     </Box>
