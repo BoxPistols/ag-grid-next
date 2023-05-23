bf272d30ffc356f80476437572a04add407ba1fb
A_ito <BoxPistols@users.noreply.github.com>
2023-05-24 00:53:50 +0900

    60 re data example covid19 on tokyo (#61)
    * API Covid-19 to Tokyo

--- src/components/AgGrid/CovidDataBasic.tsx
+++ src/components/AgGrid/CovidDataBasic.tsx
@@ -1,40 +1,46 @@
-import { useMemo, useState, useEffect } from "react"
-import { AgGridReact } from "ag-grid-react"
+import { useState, useEffect } from 'react'
+import { AgGridReact } from 'ag-grid-react'
 
-interface CountryData {
-  Country: string
-  NewConfirmed: number
-  TotalConfirmed: number
-  NewDeaths: number
-  TotalDeaths: number
-  NewRecovered: number
-  TotalRecovered: number
+interface PatientData {
+  患者_性別: string
+  退院済フラグ: string
+  患者_年代: string
+  都道府県名: string
+  患者_接触歴の有無フラグ: string
+  公表_年月日: string
+  全国地方公共団体コード: string
+  発症_年月日: string
+  確定_年月日: string
+  患者_職業: string
 }
+
 const CovidDataBasic: React.FC = () => {
-  const [rowData, setRowData] = useState<CountryData[]>([])
+  const [rowData, setRowData] = useState<PatientData[]>([])
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch("https://api.covid19api.com/summary")
+      const response = await fetch(
+        'https://api.data.metro.tokyo.lg.jp/v1/Covid19Patient'
+      )
       const data = await response.json()
-      setRowData(data.Countries)
+      setRowData(data[0])
     }
     fetchData()
   }, [])
 
   const columnDefs = [
-    { headerName: "Country", field: "Country" },
-    { headerName: "New Confirmed", field: "NewConfirmed" },
-    { headerName: "New Confirmed", field: "NewConfirmed" },
-    { headerName: "Total Confirmed", field: "TotalConfirmed" },
-    { headerName: "New Deaths", field: "NewDeaths" },
-    { headerName: "Total Deaths", field: "TotalDeaths" },
-    { headerName: "New Recovered", field: "NewRecovered" },
-    { headerName: "Total Recovered", field: "TotalRecovered" },
+    { headerName: '性別', field: '患者_性別' },
+    { headerName: '年代', field: '患者_年代' },
+    { headerName: '都道府県名', field: '都道府県名' },
+    { headerName: '接触歴の有無', field: '患者_接触歴の有無フラグ' },
+    { headerName: '公表日', field: '公表_年月日' },
+    { headerName: '発症日', field: '発症_年月日' },
+    { headerName: '確定日', field: '確定_年月日' },
+    { headerName: '職業', field: '患者_職業' },
   ]
 
   return (
-    <div className="ag-theme-alpine" style={{ height: "400px", width: "100%" }}>
+    <div className="ag-theme-alpine" style={{ height: '400px', width: '100%' }}>
       <AgGridReact
         columnDefs={columnDefs}
         rowData={rowData}
