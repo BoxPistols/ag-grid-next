7816b9491700f90d997fdf66a056a6d2b4990765
a.ito <10333049+BoxPistols@users.noreply.github.com>
2024-04-07 18:41:01 +0900

    .eslintrc.jsonのルール更新 CsvUploaderコンポーネントにデフォルトの列定義を追加

--- src/components/AgGrid/FileUpload/CsvUploader.tsx
+++ src/components/AgGrid/FileUpload/CsvUploader.tsx
@@ -7,22 +7,42 @@ const CsvUploader = () => {
   const [columnDefs, setColumnDefs] = useState<any[]>([])
   const [rowData, setRowData] = useState<any[]>([])
 
+  const defaultColDef = {
+    resizable: true,
+    flex: 1,
+    minWidth: 100,
+  }
+
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const fileList = event.target.files
     if (fileList) setFile(fileList[0])
   }
-
   const handleUpload = async () => {
     if (!file) return
-
     const reader = new FileReader()
     reader.readAsText(file)
-
     reader.onload = async () => {
       const csvData = reader.result as string
       const { headers, data } = parseCsvData(csvData)
       setColumnDefs(
-        headers.map((header: string) => ({ headerName: header, field: header }))
+        headers.map((header: string) => ({
+          headerName: header,
+          field: header,
+          filter:
+            header === 'isActive' ? 'agSetColumnFilter' : 'agTextColumnFilter',
+          comparator: (
+            valueA: number | null,
+            valueB: number | null,
+            nodeA: any,
+            nodeB: any,
+            isInverted: any
+          ) => {
+            if (valueA === null && valueB === null) return 0
+            if (valueA === null) return isInverted ? -1 : 1
+            if (valueB === null) return isInverted ? 1 : -1
+            return isInverted ? valueB - valueA : valueA - valueB
+          },
+        }))
       )
       setRowData(data)
     }
@@ -38,7 +58,6 @@ const CsvUploader = () => {
         {}
       )
     })
-
     return { headers: header, data }
   }
 
@@ -70,7 +89,11 @@ const CsvUploader = () => {
         className="ag-theme-alpine"
         style={{ height: '500px', width: '100%' }}
       >
-        <AgGridReact columnDefs={columnDefs} rowData={rowData} />
+        <AgGridReact
+          columnDefs={columnDefs}
+          rowData={rowData}
+          defaultColDef={defaultColDef}
+        />
       </div>
     </Box>
   )
